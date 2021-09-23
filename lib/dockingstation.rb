9f3d143017eba373bfd72859b600f41a27f2f144
aisha dorsett <aishadorsett23@gmail.com>
2021-09-23 22:28:05 +0100

    refactoring and setting default capacity that can change

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -1,23 +1,30 @@
 class DockingStation
-    attr_reader :bike
-    attr_accessor :bikes
-    def initialize
-        @bikes = []      
+    #allow the docking station instance to change its own cpacity after created
+    attr_accessor :capacity
+
+    DEFAULT_CAPACITY=20
+
+    def initialize(capacity = DEFAULT_CAPACITY)
+        @capacity = capacity
+        @bikes = []    
     end
 
     def release_bike
         fail 'no bikes available' if @bikes.empty?
         @bikes.pop
-        @bike = bike
-    end
-
-    def station_availablity 
-        @bikes.length
     end
 
     def docked(bike)
-        fail 'Docking station at capacity' if @bikes.count >= 20
+        fail 'Docking station at capacity' if full?
         @bikes << bike
         @bike = bike
+    end
+
+    private 
+    
+    attr_reader :bikes
+
+    def full?
+        @bikes.length == DEFAULT_CAPACITY
     end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,25 +2,34 @@ require 'dockingstation'
 
 RSpec.describe DockingStation do
 
+   describe 'DockingStation initialize' do
+      it 'allows user to set docking station capacity' do
+         dock = DockingStation.new(25)
+         expect(dock.capacity).to eq 25
+      end
+   end
+
    describe '#release_bike' do
       it 'can release a bike' do
          bike = Bike.new
          subject.docked(bike)
-         expect(subject.release_bike).to eq bike
+         expect(subject.release_bike).to be_an_instance_of(Bike)
       end
          
-      it 'returns error when no bikes are docked' do
+      it 'raises error when no bikes are docked' do
          expect {subject.release_bike}.to raise_error 'no bikes available'
       end
+   end
 
+   describe '#docked' do
       it 'can return the docked bike' do 
          bike = Bike.new
-         expect(DockingStation.new.docked(bike)).to eq bike
+         expect(subject.docked(bike)).to be_an_instance_of(Bike)
       end
 
       it 'raises an error when someone tries to dock to a station with 20 bikes' do
          bike = Bike.new
-         20.times { subject.docked Bike.new }
+         subject.capacity.times { subject.docked Bike.new }
          expect { subject.docked(bike) }.to raise_error 'Docking station at capacity'
       end
    end
